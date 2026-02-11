8a0157e1b398e3c34ca8a7a0661188c5c99eb4f4
Diken-Shrestha <138406528+Diken-Shrestha@users.noreply.github.com>
2026-02-10 22:23:21 -0700

    Add files via upload

---
+++ tests/data quality check silver/silver_quality_check_crm_prd_info.sql
@@ -0,0 +1,31 @@
+-- Check for nulls or duplicates in Primary Key
+-- Expectation: No Result
+
+SELECT prd_id, count(*) FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Check for unwanted spaces 
+-- Expectation: No Results
+SELECT prd_nm
+FROM silver.crm_prd_info
+where prd_nm != TRIM(prd_nm);
+
+-- Check for NULLs or Negative Numbers
+-- Expectation: No Results
+SELECT prd_cost FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- Check for Data Standardization & Consistency
+-- Expectations:
+
+SELECT DISTINCT prd_line 
+FROM silver.crm_prd_info;
+
+-- Check for Invalid Date Orders
+SELECT * FROM silver.crm_prd_info
+WHERE prd_end_date < prd_start_date
+
+
+
+SELECT * FROM silver.crm_prd_info
